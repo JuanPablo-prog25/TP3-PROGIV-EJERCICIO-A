e0d119ff4e61074fcf0b2b67bd772459990b9867
Juan Pablo Ocampo de la Vega <juanpabloocampo61@gmail.com>
2025-11-08 21:15:12 -0300

    se agrego las estructuras iniciales de las rutas en index.js y se configuro el servidor express

---
+++ Ejercicio-A/backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { conectarDB } from "./db.js";
+
+await conectarDB();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+authConfig();
+
+app.get("/", (req, res) => {
+  res.send("gestion de alumnos");
+});
+
+app.use("/auth", authRouter);
+app.use("/alumnos", alumnosRouter);
+app.use("/materias", materiasRouter);
+app.use("/notas", notasRouter);
+
+app.listen(port, () => {
+  console.log(`La aplicacion esta funcionando en el puerto ${port}`);
+});
