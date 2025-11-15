3b0f6d3c3f323f50691cca0839b45620e92e7615
Juan Pablo Ocampo de la Vega <juanpabloocampo61@gmail.com>
2025-11-14 23:54:26 -0300

    se agrego el router faltante en el index (usuarios)

--- Ejercicio-A/backend/index.js
+++ Ejercicio-A/backend/index.js
@@ -4,6 +4,7 @@ import { conectarDB } from "./db.js";
 import alumnosRouter from "./alumnos.js";
 import materiasRouter from "./materias.js"
 import notasRouter from "./notas.js";
+import usuariosRouter from "./usuarios.js"
 import authRouter, { authConfig } from "./auth.js";
 
 await conectarDB();
@@ -24,6 +25,7 @@ app.use("/auth", authRouter);
 app.use("/alumnos", alumnosRouter);
 app.use("/materias", materiasRouter);
 app.use("/notas", notasRouter);
+app.use("/usuarios", usuariosRouter);
 
 app.listen(port, () => {
   console.log(`La aplicacion esta funcionando en el puerto ${port}`);
